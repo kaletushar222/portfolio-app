8d13a60f0df59d6dd4de4289daefe7897fadb6ed
tushar kale <kaletushar222@gmail.com>
2025-05-13 01:15:10 +0530

    dynamic content

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,11 +1,21 @@
 import React from "react";
-import "./Portfolio.css";
+import { useState } from "react";
 import apiUrls from "../../urls/apiUrls";
+import "./Portfolio.css";
 
 const Portfolio = (props) => {
 	const applications = props.applications;
 	const photographs = props.photographs;
 	const imagesURL = apiUrls.imagesURL;
+	const [filterSelected, setFilterSelected] = useState("ALL");
+
+	const handleFilterSelection = (filter) => {
+		setFilterSelected(filter);
+	};
+
+	// ease in and out animation when we switch between filters
+	
+
 	return (
 		<div>
 			<section id="portfolio" className="portfolio section">
@@ -18,41 +28,45 @@ const Portfolio = (props) => {
 					<div className="isotope-layout" data-default-filter="*" data-layout="masonry" data-sort="original-order">
 
 						<ul className="portfolio-filters isotope-filters" data-aos="fade-up" data-aos-delay="100">
-							<li data-filter="*" className="filter-active">All</li>
-							<li data-filter=".filter-app">Applications</li>
-							<li data-filter=".filter-product">Photographs</li>
+							<li onClick={ () => handleFilterSelection("ALL") } data-filter="*" className={ filterSelected == "ALL" && "filter-active" } >All</li>
+							<li onClick={ () => handleFilterSelection("APPS") } data-filter=".filter-app" className={ filterSelected == "APPS" && "filter-active" } >Applications</li>
+							<li onClick={ () => handleFilterSelection("PHOTOS") } data-filter=".filter-product" className={ filterSelected == "PHOTOS" && "filter-active" }>Photographs</li>
 						</ul>
 
 						<div className="row gy-4 isotope-container" data-aos="fade-up" data-aos-delay="200">
 							{
 								applications.map((app, key) => {
-									return (
-										<div key={key} className="col-lg-4 col-md-6 portfolio-item isotope-item filter-app">
-											<img style={{width: "100%"}} src={ imagesURL + app.imageUrl} alt={app.title} />
-											<div className="portfolio-info">
-												<h4>{app.title}</h4>
-												<p>{app.description}</p>
-												{ app.appUrl && <span> <a href={ app.appUrl } target="_blank" > Visit App </a> </span> }
-												<a href={imagesURL + app.imageUrl} title={app.title} className="glightbox preview-link"><i className="bi bi-zoom-in"></i></a>
+									if (filterSelected == "ALL" || filterSelected === "APPS") {
+										return (
+											<div key={key} className="col-lg-4 col-md-6 portfolio-item isotope-item filter-app">
+												<img style={{width: "100%"}} src={ imagesURL + app.imageUrl} alt={app.title} />
+												<div className="portfolio-info">
+													<h4>{app.title}</h4>
+													<p>{app.description}</p>
+													{ app.appUrl && <span> <a href={ app.appUrl } target="_blank" > Visit App </a> </span> }
+													<a href={imagesURL + app.imageUrl} title={app.title} className="glightbox preview-link"><i className="bi bi-zoom-in"></i></a>
+												</div>
 											</div>
-										</div>
-									)
+										)
+									}
 								})
 							}
 							{
 								photographs.map((photoObj, key) => {
-									return (
-										<div key={key} className="col-lg-4 col-md-6 portfolio-item isotope-item filter-product">
-											<img style={{width: "100%"}} src={ imagesURL + photoObj.imageUrl } className="img-fluid" alt={ photoObj.title } />
-											<div className="portfolio-info">
-												<h4>{ photoObj.title }</h4>
-												<p> { photoObj.description }</p>
-												<a href={ imagesURL + photoObj.imageUrl } title={ photoObj.title } className="glightbox preview-link"><i className="bi bi-zoom-in"></i></a>
+									if (filterSelected == "ALL" || filterSelected === "PHOTOS") {
+										return (
+											<div key={key} className="col-lg-4 col-md-6 portfolio-item isotope-item filter-product">
+												<img style={{width: "100%"}} src={ imagesURL + photoObj.imageUrl } className="img-fluid" alt={ photoObj.title } />
+												<div className="portfolio-info">
+													<h4>{ photoObj.title }</h4>
+													<p> { photoObj.description }</p>
+													<a href={ imagesURL + photoObj.imageUrl } title={ photoObj.title } className="glightbox preview-link"><i className="bi bi-zoom-in"></i></a>
+												</div>
 											</div>
-										</div>
-									)
+										)
+									}
 								})
-              }
+              				}
 						</div>
 					</div>
 				</div>
