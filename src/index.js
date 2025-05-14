7e9a17644afe5f10147744e041f099f02c8c5126
tushar kale <kaletushar222@gmail.com>
2025-05-15 02:02:18 +0530

    cookie changes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { clarity } from "clarity-js";
+import config from "./config.json";
+
+const startClarityIfAccepted = () => {
+  const cookiesAccepted = localStorage.getItem("cookiesAccepted") === "true";
+  if (cookiesAccepted) {
+    clarity.start(config["clarity-project-id"]);
+  }
+};
+
+startClarityIfAccepted();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
